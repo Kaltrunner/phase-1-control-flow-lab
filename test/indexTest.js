95f66386f020aada4c31d2fc4154376432f4bcba
Erik Russell <erikrussells@gmail.com>
2022-09-19 13:31:01 -0400

    commit tree 800138f8be1d771112ed794f6301fdff966ffec5

--- test/indexTest.js
+++ test/indexTest.js
@@ -36,7 +36,7 @@ describe('index.js', function () {
       expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
     });
 
-    it('should return "Bye." if anything else', function () {
+    it('should return "Bye."if anything else ', function () {
       expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
     });
   });
